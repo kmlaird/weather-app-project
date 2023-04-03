1eeb9b6c3ac0ada0ecb1ed6ce06019945f0b769d
kmlaird <kmlaird15@gmail.com>
2023-04-03 16:49:53 -0700

    JS connection fix and date/time js added

---
+++ src/script.js
@@ -0,0 +1,35 @@
+//Last Update Date/Time
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tueday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let hour = now.getHours();
+let minute = now.getMinutes();
+
+let newDate = document.querySelector(`#date-updated`);
+newDate.innerHTML = `Last Updated: ${day}, ${month} ${date} at ${hour}:${minute}`;
