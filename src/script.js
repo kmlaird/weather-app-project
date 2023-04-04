c56c7b531ed64f928aa08079da21ecf6abb1cd12
kmlaird <kmlaird15@gmail.com>
2023-04-03 20:18:09 -0700

    search engine and current location functionality added

--- src/script.js
+++ src/script.js
@@ -33,3 +33,44 @@ let minute = now.getMinutes();
 
 let newDate = document.querySelector(`#date-updated`);
 newDate.innerHTML = `Last Updated: ${day}, ${month} ${date} at ${hour}:${minute}`;
+
+//Search and Current Temperature
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector(`.search-input`);
+  searchCity(cityInput.value);
+}
+let cityForm = document.querySelector(`#city-form`);
+cityForm.addEventListener(`submit`, search);
+
+function showPosition(position) {
+  let lon = position.coords.longitude;
+  let lat = position.coords.latitude;
+
+  let apiKey = `de78cc20aef9bef799c814cc01fdc85f`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(currentTemp);
+}
+
+function currentTemp(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temp = document.querySelector(`#today-temp`);
+  let cityName = response.data.name;
+  let heading = document.querySelector(`#city-country`);
+  temp.innerHTML = `${temperature}`;
+  heading.innerHTML = `${cityName}`;
+}
+
+function searchCity(city) {
+  let apiKey = `de78cc20aef9bef799c814cc01fdc85f`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(currentTemp);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let localeButton = document.querySelector(".btn-light");
+localeButton.addEventListener(`click`, getCurrentLocation);
